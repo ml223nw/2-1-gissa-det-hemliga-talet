f1b93136c748242c25b48f7b4a065bf11bb3c0b8
Mathias Lundberg <tias86@gmail.com>
2014-10-08 22:24:26 +0200

    Finished

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -15,7 +15,7 @@ namespace _1DV402.S2.L1A
         public void Initialize()
         {
             Random randomNumber = new Random();
-            _number = randomNumber.Next(1, 100);
+            _number = randomNumber.Next(1, 101);
             _count = 0;
         }
 
@@ -26,11 +26,47 @@ namespace _1DV402.S2.L1A
 
         public bool MakeGuess(int number)
         {
-            if (_number >= MaxNumberOfGuesses)
+            if (_count >= MaxNumberOfGuesses)
+
 	            {
 		             throw new ApplicationException();
 	            }
-                        if (_number < 1 || _number > 100)
+                        if (number < 1 || number > 100)
 	            {
 		             throw new ArgumentOutOfRangeException();
 	            }
+
+                if (number < _number)
+                {
+                    _count = _count + 1;
+                    Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count));
+
+                    if (_count == MaxNumberOfGuesses)
+                    {
+                        Console.WriteLine("Det hemliga talet är {0}", _number);
+                    }
+                    return false;
+                }
+                else if (number > _number)
+                {
+                    _count = _count + 1;
+                    Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count));
+
+                    if (_count == MaxNumberOfGuesses)
+                    {
+                        Console.WriteLine("Det hemliga talet är {0}", _number);
+                    }
+                    return false;
+                }
+                else
+                {
+                    _count = _count + 1;
+                    Console.WriteLine("RÄTT GISSAT! Du klarade det på {0} försök", _count);
+                    return true;
+                }
+
+            }
+
+        }
+
+    }
